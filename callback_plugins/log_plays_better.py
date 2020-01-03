821f4ff2a634a8839540d47e3007cbe8f539c17b
expressed <mmatyus1@binghamton.edu>
2020-01-02 16:57:56 -0800

    start logger

---
+++ callback_plugins/log_plays_better.py
@@ -0,0 +1,125 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = '''
+    callback: log_plays_better
+    type: notification
+    short_description: write playbook output to log file
+    version_added: historical
+    description:
+      - This callback writes playbook output to a file per host in the `/var/log/ansible/hosts` directory
+    requirements:
+     - Whitelist in configuration
+     - A writeable /var/log/ansible/hosts directory by the user executing Ansible on the controller
+    options:
+      log_folder:
+        version_added: '2.9'
+        default: /var/log/ansible/hosts
+        description: The folder where log files will be created.
+        env:
+          - name: ANSIBLE_LOG_FOLDER
+        ini:
+          - section: callback_log_plays_better
+            key: log_folder
+      log_format:
+        version_added: '2.9'
+        default: yaml
+        description: The folder where log files will be created.
+        env:
+          - name: ANSIBLE_LOG_FORMAT
+        ini:
+          - section: callback_log_plays_better
+            key: log_format
+
+'''
+
+import os
+import time
+import json
+import yaml
+
+from ansible.utils.path import makedirs_safe
+from ansible.module_utils._text import to_bytes
+from ansible.module_utils.common._collections_compat import MutableMapping
+from ansible.parsing.ajson import AnsibleJSONEncoder
+from ansible.plugins.callback import CallbackBase
+
+
+# NOTE: in Ansible 1.2 or later general logging is available without
+# this plugin, just set ANSIBLE_LOG_PATH as an environment variable
+# or log_path in the DEFAULTS section of your ansible configuration
+# file.  This callback is an example of per hosts logging for those
+# that want it.
+
+
+class CallbackModule(CallbackBase):
+    """
+    logs playbook results, per host, in /var/log/ansible/hosts
+    """
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'notification'
+    CALLBACK_NAME = 'log_plays_better'
+    CALLBACK_NEEDS_WHITELIST = True
+
+    TIME_FORMAT = "%b %d %Y %H:%M:%S"
+    MSG_FORMAT = "%(now)s - %(category)s - %(data)s\n\n"
+
+    def __init__(self):
+
+        super(CallbackModule, self).__init__()
+
+    def set_options(self, task_keys=None, var_options=None, direct=None):
+        super(CallbackModule, self).set_options(task_keys=task_keys, var_options=var_options, direct=direct)
+
+        self.log_folder = self.get_option("log_folder")
+        self.log_format = self.get_option("log_format")
+
+        if not os.path.exists(self.log_folder):
+            makedirs_safe(self.log_folder)
+
+    def log(self, host, category, data):
+        if isinstance(data, MutableMapping):
+            #if '_ansible_verbose_override' in data:
+            #    # avoid logging extraneous data
+            #    data = 'omitted'
+            #else:
+            data = data.copy()
+            invocation = data.pop('invocation', None)
+            #if self.log_format == "json":
+            #  data = json.dumps(data, cls=AnsibleJSONEncoder)
+            #elif self.log_format == "yaml":
+            #  data = yaml.dump(data)                
+
+            #if invocation is not None:
+            #  if self.log_format == "json":
+            #    data = json.dumps(invocation) + " => %s " % data
+            #  elif self.log_format == "yaml":
+            #    data = yaml.dump(invocation) + " => %s " % data
+        
+        path = os.path.join(self.log_folder, host)
+        now = time.strftime(self.TIME_FORMAT, time.localtime())
+
+        msg = to_bytes(self.MSG_FORMAT % dict(now=now, category=category, data=data))
+        with open(path, "ab") as fd:
+            fd.write(msg)
+
+    def runner_on_failed(self, host, res, ignore_errors=False):
+        self.log(host, 'FAILED', res)
+
+    def runner_on_ok(self, host, res):
+        self.log(host, 'OK', res)
+
+    def runner_on_skipped(self, host, item=None):
+        self.log(host, 'SKIPPED', '...')
+
+    def runner_on_unreachable(self, host, res):
+        self.log(host, 'UNREACHABLE', res)
+
+    def runner_on_async_failed(self, host, res, jid):
+        self.log(host, 'ASYNC_FAILED', res)
+
+    def playbook_on_import_for_host(self, host, imported_file):
+        self.log(host, 'IMPORTED', imported_file)
+
+    def playbook_on_not_import_for_host(self, host, missing_file):
+        self.log(host, 'NOTIMPORTED', missing_file)
